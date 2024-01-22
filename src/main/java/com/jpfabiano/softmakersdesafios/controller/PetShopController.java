a0a7c7769d0698a262ae1e4284cad81e2b19c717
João <jpfabiano08@gmail.com>
2024-01-22 12:14:01 -0300

    Controller pronto (Commit final)

--- src/main/java/com/jpfabiano/softmakersdesafios/controller/PetShopController.java
+++ src/main/java/com/jpfabiano/softmakersdesafios/controller/PetShopController.java
@@ -1,7 +1,10 @@
 package com.jpfabiano.softmakersdesafios.controller;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.jpfabiano.softmakersdesafios.dtos.DonoDTO;
+import com.jpfabiano.softmakersdesafios.dtos.DonoUpdateDTO;
 import com.jpfabiano.softmakersdesafios.dtos.PetDTO;
+import com.jpfabiano.softmakersdesafios.dtos.PetUpdateDTO;
 import com.jpfabiano.softmakersdesafios.models.DonoModel;
 import com.jpfabiano.softmakersdesafios.models.PetModel;
 import com.jpfabiano.softmakersdesafios.repository.DonoRepository;
@@ -13,10 +16,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.beans.Beans;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PetShopController {
@@ -36,6 +40,68 @@ public class PetShopController {
         PetModel petModel = new PetModel();
         BeanUtils.copyProperties(petDto,petModel);
         return ResponseEntity.status(HttpStatus.OK).body(petrepository.save(petModel));
+    }
+    @GetMapping ("/api_petshop/pet")
+    public ResponseEntity<List<PetModel>> mostrarPets(){
+        return ResponseEntity.status(HttpStatus.OK).body(petrepository.findAll());
+    }
+    @PutMapping ("/api_petshop/dono/{id}")
+    public ResponseEntity<Object> atualizarDono (@PathVariable (name = "id") Long id , @RequestBody DonoUpdateDTO donoUpdateDto){
+        Optional<DonoModel> dono = donorepository.findById(id);
+        if (dono.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O dono com esse id não foi encontrado");
+        DonoModel donoModel = dono.get();
+        if (donoUpdateDto.nome() != null){
+            donoModel.setNome(donoUpdateDto.nome());
+        }
+        if (donoUpdateDto.idade() != null){
+            donoModel.setIdade(donoUpdateDto.idade());
+        }
+        if (donoUpdateDto.phone() != null){
+            donoModel.setPhone(donoUpdateDto.phone());
+        }
+        if (donoUpdateDto.bairro() != null){
+            donoModel.setBairro(donoUpdateDto.bairro());
+        }
+        if (donoUpdateDto.rua() != null){
+            donoModel.setRua(donoUpdateDto.rua());
+        }
+        if (donoUpdateDto.numero() != null){
+            donoModel.setNumero(donoUpdateDto.numero());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(donorepository.save(donoModel));
+    }
+    @PutMapping ("/api_petshop/pet/{id}")
+    public ResponseEntity<Object> atualizarPet (@PathVariable (name = "id") Long id, @RequestBody @Validated PetUpdateDTO petUpdateDto){
+        Optional<PetModel> pet = petrepository.findById(id);
+        if (pet.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O pet com esse id não foi encontrado");
+        PetModel petModel = pet.get();
+        if (petUpdateDto.nome() != null){
+            petModel.setNome(petUpdateDto.nome());
+        }
+        if (petUpdateDto.idade() != null){
+            petModel.setIdade(petUpdateDto.idade());
+        }
+        if (petUpdateDto.especie() != null){
+            petModel.setEspecie(petUpdateDto.especie());
+        }
+        if (petUpdateDto.raca() != null){
+            petModel.setRaca(petUpdateDto.raca());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(petrepository.save(petModel));
+    }
+    @DeleteMapping ("/api_petshop/dono/{id}")
+    ResponseEntity<Object> deletarDono (@PathVariable(name = "id") Long id){
+        Optional<DonoModel> dono = donorepository.findById(id);
+        if (dono.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O dono com esse id não foi encontrado");
+        donorepository.delete(dono.get());
+        return ResponseEntity.status(HttpStatus.OK).body(dono);
+    }
+    @DeleteMapping("/api_petshop/pet/{id}")
+    ResponseEntity<Object> deletarPet (@PathVariable (name = "id") Long id){
+        Optional<PetModel> pet = petrepository.findById(id);
+        if (pet.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O pet com esse id não foi encontrado");
+        petrepository.delete(pet.get());
+        return ResponseEntity.status(HttpStatus.OK).body(pet);
     }
 
 }
